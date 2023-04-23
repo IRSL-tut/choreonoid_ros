08e4ae3213b487655e5e94b56049ce1b0bbe635e
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-04-23 15:27:25 +0900

    [IRSL patch] add ROS-Items to python-bindings

--- src/plugin/BodyROSItem.h
+++ src/plugin/BodyROSItem.h
@@ -61,6 +61,22 @@ public:
 
     void setModuleName(const std::string& name);
 
+    void setNameSpace(const std::string& _nm)
+    {
+        name_space = _nm;
+    }
+    const std::string &getNameSpace()
+    {
+        return name_space;
+    }
+    void setJointStateUpdateRate(double _rate)
+    {
+        jointStateUpdateRate = _rate;
+    }
+    double getJointStateUpdateRate()
+    {
+        return jointStateUpdateRate;
+    }
 protected:
     virtual Item* doDuplicate() const override;
     virtual bool store(Archive& archive) override;
@@ -89,7 +105,7 @@ private:
     double controlTime_;
     std::ostream& os;
 
-    std::string bodyName;
+    std::string name_space;
 
     std::unique_ptr<ros::NodeHandle> rosNode;
 
